d6427e9672c206e767545e40bb72ec03d255f420
Apache-3 <nurzanbekovemir@gmail.com>
2024-01-10 22:55:26 +0600

    sec commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,13 @@ export const Footer = () => {
     return (
         <div>
             footer
+            <ul>
+                <li>home</li>
+                <li>about us</li>
+                <li>contacts</li>
+                <li>map</li>
+            </ul>
         </div>
+
     )
 }
